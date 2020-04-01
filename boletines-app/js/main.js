5e26bce8bf0021e496b55a666a4b89f90aec02a0
Braulio Soifer <brauliosoifer@gmail.com>
2020-04-01 04:15:25 -0500

    Agregamos Boletines-app

---
+++ boletines-app/js/main.js
@@ -0,0 +1,50 @@
+//DOM
+/*let links = document.querySelectorAll("a");
+links.forEach(function(link) {
+  console.log(link);
+});
+*/
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link) {
+  link.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    let content = document.querySelector(".content");
+    //quitar clase  animated
+    content.classList.remove("bounceInRight");
+    content.classList.remove("animated");
+
+    //agregar clase animated
+    content.classList.add("bounceOutLeft");
+    content.classList.add("animated");
+
+    setTimeout(function() {
+      location.href = "/";
+    }, 600);
+
+    return false;
+  });
+});
+
+let vistas = document.querySelectorAll(".ver");
+
+vistas.forEach(function(vista) {
+  vista.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    let content = document.querySelector(".content");
+    //quitar clase  animated
+    content.classList.remove("bounceInRight");
+    content.classList.remove("animated");
+
+    //agregar clase animated
+    content.classList.add("bounceOutLeft");
+    content.classList.add("animated");
+
+    setTimeout(function() {
+      location.href = "./animes/fma.html";
+    }, 600);
+
+    return false;
+  });
+});
